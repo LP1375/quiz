3fabdc72764137f4d3cc1954893914b57444e3bd
Vlad <f.a.s.t.i.k.19801@gmail.com>
2023-02-17 15:27:20 +0100

    q

--- src/App.jsx
+++ src/App.jsx
@@ -94,7 +94,7 @@ function App() {
         showScore
           ? <div className="section_score">
               <div>
-                Правильних відповідей {score} з {questions.length}. <br /> А тепер включи відео
+                Правильних відповідей {score} з {questions.length}. <br /> А тепер включи відео.
               </div>
               <ReactPlayer url='https://www.youtube.com/watch?v=rnjbemXGNmw&ab_channel=%D0%9F%D0%BE%D0%B4%D0%BF%D0%B8%D1%88%D0%B8%D1%81%D1%8C%D0%BD%D0%B0%D0%BA%D0%B0%D0%BD%D0%B0%D0%BB%D0%91%D0%A0%D0%90%D0%A2%D0%92%D0%90%21' width='448px' height='283,5px' />
             </div>
